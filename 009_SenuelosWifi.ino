b55604bc346c1107a16dadd609f0652f8ac2d318
Jose Piñeiro <jose.valenzuela.3197@gmail.com>
2024-03-30 13:43:04 +0100

    2 ssid con mismo nombre

--- 009_SenuelosWifi.ino
+++ 009_SenuelosWifi.ino
@@ -1,12 +1,16 @@
 #include <ESP8266WiFi.h>
 
-#define SSID_SIZE   0x0E
+#define SSID_SIZE    0x0E
+#define SSID_SIZE1   0x0F
 
 extern "C" {
   #include "user_interface.h"
 }
 
 String ssid = "Red no segura";
+String ssid2 = "Red no segura ";
+String ssid3 = "Red no segura";
+int ssid_sel=1;
 byte channel;
 
 uint8_t packet[128] = { 0x80, 0x00, 0x00, 0x00, 
@@ -21,10 +25,26 @@ uint8_t packet[128] = { 0x80, 0x00, 0x00, 0x00,
                         0x00, SSID_SIZE, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72,
                         0x01, 0x08, 0x82, 0x84,
                         0x8b, 0x96, 0x24, 0x30, 0x48, 0x6c, 0x03, 0x01, 
-                        0x04};                       
+                        0x04};  
+
+uint8_t packet1[128] = { 0x80, 0x00, 0x00, 0x00, 
+                        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 
+                        0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
+                        0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 
+                        0xc0, 0x6c, 
+                        0x83, 0x51, 0xf7, 0x8f, 0x0f, 0x00, 0x00, 0x00, 
+                        0x64, 0x00, 
+                        0x01, 0x04, 
+                
+                        0x00, SSID_SIZE1, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72,
+                        0x01, 0x08, 0x82, 0x84,
+                        0x8b, 0x96, 0x24, 0x30, 0x48, 0x6c, 0x03, 0x01, 
+                        0x04};
+                                     
 
 
 void setup() {
+  Serial.begin(9600);
   delay(500);
   wifi_set_opmode(STATION_MODE);
   wifi_promiscuous_enable(1); 
@@ -43,25 +63,41 @@ void loop() {
     packet[14] = packet[20] = random(256);
     packet[15] = packet[21] = random(256);
 
-    packet[38] = ssid[0];
-    packet[39] = ssid[1];
-    packet[40] = ssid[2];
-    packet[41] = ssid[3];
-    packet[42] = ssid[4];
-    packet[43] = ssid[5];
-    packet[44] = ssid[6];
-    packet[45] = ssid[7];
-    packet[46] = ssid[8];
-    packet[47] = ssid[9];
-    packet[48] = ssid[10];
-    packet[49] = ssid[11];
-    packet[50] = ssid[12];
-    packet[51] = ssid[13];
+    packet1[10] = packet1[16] = random(256);
+    packet1[11] = packet1[17] = random(256);
+    packet1[12] = packet1[18] = random(256);
+    packet1[13] = packet1[19] = random(256);
+    packet1[14] = packet1[20] = random(256);
+    packet1[15] = packet1[21] = random(256);
+
+    switch(ssid_sel){
+      case 1:
+        Serial.println ("SSID");
+        for (int i = 0; i<SSID_SIZE; i++){
+              packet[38+i] =  ssid[i];
+              wifi_send_pkt_freedom(packet, 51 + SSID_SIZE, 0);
+              wifi_send_pkt_freedom(packet, 51 + SSID_SIZE, 0);
+              wifi_send_pkt_freedom(packet, 51 + SSID_SIZE, 0);
+        }
+        break;
+      case 2:
+        Serial.println ("SSID 1");
+        for (int i = 0; i<SSID_SIZE1; i++){
+            packet1[38+i] =  ssid2[i];
+            wifi_send_pkt_freedom(packet1, 51 + SSID_SIZE1, 0);
+            wifi_send_pkt_freedom(packet1, 51 + SSID_SIZE1, 0);
+            wifi_send_pkt_freedom(packet1, 51 + SSID_SIZE1, 0);
+        }
+        break;
+    }
+    ssid_sel++;
+    if ( ssid_sel == 3 ){
+      ssid_sel = 1;
+    }
     
     packet[56] = channel;
+    packet1[56] = channel;
     
-    wifi_send_pkt_freedom(packet, 51 + SSID_SIZE, 0);
-    wifi_send_pkt_freedom(packet, 51 + SSID_SIZE, 0);
-    wifi_send_pkt_freedom(packet, 51 + SSID_SIZE , 0);
+    
     delay(1);
 }
